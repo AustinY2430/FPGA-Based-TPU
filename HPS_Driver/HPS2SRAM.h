3b7c13b711fb34a50f0ae63b03471a48b39df6ad
Bradley Manzo <76194522+Brmanzo@users.noreply.github.com>
2024-10-17 19:46:51 -0700

    Fixed Typo

--- HPS_Driver/HPS2SRAM.h
+++ HPS_Driver/HPS2SRAM.h
@@ -11,7 +11,7 @@
 /* to write per tile according to the sparse matrices generated from earlier */
 int sparse_element_prediction(int sparse_a_column, int sparse_b_row, int TilesPerOp, char *sparse_a, char *sparse_b);
 
-/* Accumates bytes into a word, writes to SRAM and flushes buffer when full. */
+/* Accumulates bytes into a word, writes to SRAM and flushes buffer when full. */
 void SRAM_ACC_Write(volatile unsigned int **sram_ptr_ptr,
     uint32_t *ACC, uint8_t ByteToACC);
 
@@ -21,6 +21,7 @@ void tile_offset_updater(int *a_column, int *a_row, int *sparse_a_column,
     int *b_column, int *b_row, int *sparse_b_row, int AH, int AW, int BH,
         int *a_offset, int *b_offset, int OutputTile);
 
+/* Contains the main operation of the TPU sending tile data, and reading results. */
 int HPS_ENGINE(uint8_t **matrix_a, uint8_t **matrix_b, int tile_size,
 	int AH, int AW, int BH, int BW, unsigned long long *acc_hps_time,
 		unsigned long long *acc_fpga_time, int *empty_columns_a, int *empty_rows_b,
